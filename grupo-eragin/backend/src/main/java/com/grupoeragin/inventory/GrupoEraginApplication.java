3ba103f3936322b61f6fa73c1de86637a24a50ef
Maitane González <mgGonzalez.dev@gmail.com>
2026-06-15 13:39:49 +0200

    Creación de la estructura base del frontend, backend y docker-compose

---
+++ grupo-eragin/backend/src/main/java/com/grupoeragin/inventory/GrupoEraginApplication.java
@@ -0,0 +1,13 @@
+package com.grupoeragin.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoEraginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrupoEraginApplication.class, args);
+    }
+
+}
---
+++ grupo-eragin/backend/src/test/java/com/grupoeragin/inventory/GrupoEraginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoeragin.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoEraginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
